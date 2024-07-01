6972a3742cdfb67f25606f278ff3804b57e10cbb
kanuki00 <kalevi.toivanen@gmail.com>
2024-07-01 22:12:34 +0300

    pslb: rewrite. making good progress

--- pslb_tool/pslb.py
+++ pslb_tool/pslb.py
@@ -1,193 +1,172 @@
 import sys
-import string
 
+##################
 printbytes = []
 my_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_@'
 for c in my_chars:
     printbytes.append(c.encode('utf-8'))
+##################
+'''
+The string type is a combination of an uint32 for the length of 
+the string followed by the characters of the string.
 
-def get_valid_magic(filename):
-    ifm=open(filename, "rb")
-    magic = ifm.read(4).decode('utf-8') # read first 4 bytes and decode to string
-    ifm.close()
-    if magic == 'PSLB': 
-        return True
-    else:
-        return False
-        
-def int_from_loose_bytes(bytes, little_endian):
+For the Map and List after the type a uint32 follows that 
+represents the number of entries in the Map/List.
+
+The keys of the Map are always strings but the values can have 
+any type of the table 
+
+0x01 = String
+0x02 = int32
+0x03 = float (4 byte)
+0x04 = Byte
+0x05 = Map
+0x06 = List
+'''
+##################
+
+comp = 0            # comp = compose
+string_flag = False
+int32_flag = False
+float_flag = False
+byte_flag = False
+map_flag = False
+list_flag = False
+
+int32_literal = 0
+possible_string_flag = False
+map_key_flag = False
+     
+def iflb(bytes, little_endian): # iflb = int from loose bytes
     intarr = []
     for byte in bytes:
         intarr.append(int.from_bytes(byte))
-
     if little_endian:
         intarr.reverse()
-
     arr = bytearray(intarr)
     return int(arr.hex(), 16)
-    
-
 
 def convert(filename):
-    if get_valid_magic(filename):
-        infile = open(filename, "rb") # read bytes
-        ba = bytearray(infile.read())
-        #######
-        skip = 0
-        struct_name_stored = False
-        structure_name_store = ""
-        string_flag = False
-        int_flag = False
-        int_int_byte_count = 0
-        int_int_bytes = []
-        map_flag = False
-        map_int_byte_count = 0
-        map_int_bytes = []
-        list_flag = False
-        list_int_byte_count = 0
-        list_int_bytes = []
-        for i in range(16, len(ba)): # start at offset 0x10 to cut out header
-            byte = ba[i].to_bytes(1)
-            
-            #######
-            if skip > 0:
-                skip -= 1
-                continue
-                
-            if byte == bytes.fromhex('00') and string_flag == False and int_flag == False and map_flag == False and list_flag == False:
-                #print("filler zero")
-                continue
-            
-            if byte != bytes.fromhex('00') and byte not in printbytes:
-                if i+4+1 > len(ba):
-                    continue
-                next_byte = ba[i+1].to_bytes(1)
-                string_start_byte = ba[i+4].to_bytes(1)
-                possible_str_len = int_from_loose_bytes([byte, next_byte], True)
-                if string_start_byte in printbytes:
-                    intarr = []
-                    fail = False
-                    for j in range(possible_str_len):
-                        if i+j+4+1 > len(ba):
-                            fail = True
-                            break
-                        sbyte = ba[i+j+4].to_bytes(1)
-                        if sbyte not in printbytes:
-                            fail = True
-                            break
-                        intarr.append(int.from_bytes(sbyte))
-                    if not fail:
-                        if string_flag:
-                            print("%s (string length: %1d)" % (bytearray(intarr).decode('utf-8'), possible_str_len))
-                            string_flag = False
-                            skip = possible_str_len+3
-                        else:
-                            struct_name_stored = True
-                            structure_name_store = bytearray(intarr).decode('utf-8')
-                            skip = possible_str_len+3
-                    continue
-            if byte == bytes.fromhex('01') and map_flag == False and list_flag == False:
-                string_flag = True
-                continue
-                    
-            if byte == bytes.fromhex('02') and string_flag == False and map_flag == False and list_flag == False:
-                int_flag = True
-                continue
-            if int_flag and int_int_byte_count < 2:
-                int_int_bytes.append(byte)
-                int_int_byte_count += 1    
-                if int_int_byte_count == 2:
-                    print('Int32, %3d' % (int_from_loose_bytes(int_int_bytes, True)))
-                    int_flag = False
-                    int_int_byte_count = 0
-                    int_int_bytes = []
-                    continue
-                continue
-                           
-            if byte == bytes.fromhex('05') and string_flag == False and int_flag == False and list_flag == False:
-                map_flag = True
-                continue
-            if map_flag and map_int_byte_count < 2:
-                map_int_bytes.append(byte)
-                map_int_byte_count += 1    
-                if map_int_byte_count == 2:
-                    if struct_name_stored:
-                        print('Map %s, %3d entries' % (structure_name_store, int_from_loose_bytes(map_int_bytes, True)))
-                        struct_name_stored = False
-                        structure_name_store = ""
-                    else:
-                        print('Map, %3d entries' % (int_from_loose_bytes(map_int_bytes, True)))
-                    map_flag = False
-                    map_int_byte_count = 0
-                    map_int_bytes = []
-                    continue
-                continue
-                
-            if byte == bytes.fromhex('06') and string_flag == False and int_flag == False and map_flag == False:
-                list_flag = True
-                continue
-            if list_flag and list_int_byte_count < 2:
-                list_int_bytes.append(byte)
-                list_int_byte_count += 1    
-                if list_int_byte_count == 2:
-                    if struct_name_stored:
-                        print('List %s, %3d entries' % (structure_name_store, int_from_loose_bytes(list_int_bytes, True)))
-                        struct_name_stored = False
-                        structure_name_store = ""
-                    else:
-                        print('List, %3d entries' % (int_from_loose_bytes(list_int_bytes, True)))
-                    list_flag = False
-                    list_int_byte_count = 0
-                    list_int_bytes = []
-                    continue
-                continue 
-            
-            #print(byte.hex())
-            print(byte)
-            
-        infile.close()
-    else:
-        print("Invalid magic word!")
+    file = open(filename, "rb")
+    fba = bytearray(file.read())    # file byte array
+    fba_len = len(fba)              # file byte array length
+    
+    global comp
+    global string_flag
+    global int32_flag
+    global byte_flag
+    global list_flag
+    global map_key_flag
+    global possible_string_flag
+    
+    for i in range(16, fba_len):    # ignoring header by starting i at 16
+        byte = fba[i].to_bytes(1)
         
-def bytedump(filename): # debug
-    infile = open(filename, "rb") # read bytes
-    ba = bytearray(infile.read())
-    for b in ba:
-        byte = b.to_bytes(1)
-        print(byte)
-    infile.close()
+        if comp > 0:
+            comp -= 1
+            
+        # Checking byte
+        if byte == b'\x01' and comp == 0:
+            possible_string_flag = True
+        if byte != b'\x00' and comp == 0 and byte not in printbytes:
+            stage1 = True
+            if i+1 >= fba_len:
+                stage1 = False
+            if i+4 >= fba_len:
+                stage1 = False
+            if stage1:
+                nxbyte = fba[i+1].to_bytes(1)
+                pstrlen = iflb([byte, nxbyte], True)        # possible string length
+                ssbyte = fba[i+4].to_bytes(1)               # string start byte
+#                 try:
+#                     print("pstrlen = %s, startbyte = %s" % (str(pstrlen), ssbyte.decode('utf-8')))
+#                 except UnicodeDecodeError:
+#                     print("pstrlen = %s, Unicode decode error" % (str(pstrlen)))
+                
+                stage2 = True  
+                if i+3+pstrlen >= fba_len:
+                    stage2 = False
+                if ssbyte not in printbytes:
+                    stage2 = False
+                if stage2:
+                    stage3 = True
+                    for j in range(pstrlen):
+                        testbyte = fba[i+4+j].to_bytes(1)
+                        if testbyte not in printbytes:
+                            stage3 = False
+#                        print(testbyte.decode('utf-8'))
+                    if stage3 and possible_string_flag:
+                        string_flag = True
+                        possible_string_flag = False
+                    elif stage3:
+                        map_key_flag = True
+        if byte == b'\x02' and comp == 0 and not map_key_flag:
+            int32_flag = True
+            stage1 = True
+            if i+4 >= fba_len:
+                stage1 = False
+            if stage1:
+                db1 = fba[i+1].to_bytes(1)
+                db2 = fba[i+2].to_bytes(1)
+                db3 = fba[i+3].to_bytes(1)
+                db4 = fba[i+4].to_bytes(1)
+                int32_literal = iflb([db1, db2, db3, db4], True)
+            comp = 4
+        if byte == b'\x04' and comp == 0 and not map_key_flag:
+            byte_flag = True
+            comp = 2
+        if byte == b'\x05' and comp == 0 and not map_key_flag:
+            map_flag = True
+            comp = 4
+        if byte == b'\x06' and comp == 0 and not map_key_flag:
+            list_flag = True 
+            comp = 4
+            
+        # Printing
+        if byte in printbytes and not int32_flag:
+            print(byte.decode('utf-8'))
+        else:
+            type = ""
+            # INT32
+            if int32_flag and comp == 4:
+                type = "int32 (value=%d)" % (int32_literal)
+                int32_literal = 0
+            if int32_flag and comp == 0:
+                type = "int32 end"
+                int32_flag = False
+            # BYTE
+            if byte_flag and comp == 2:
+                type = "Byte"
+            if byte_flag and comp == 1:
+                type = "Byte end"
+                byte_flag = False
+            # MAP
+            if map_flag and comp == 4:
+                type = "Map"
+            if map_flag and comp == 0:
+                type = "Map end"
+                map_flag = False
+            # LIST            
+            if list_flag and comp == 4:
+                type = "List"
+            if list_flag and comp == 0:
+                type = "List end"
+                list_flag = False
 
+                
+            if string_flag:
+                type = "String"
+                string_flag = False            
+            if map_key_flag:
+                type = "Map key"
+                map_key_flag = False
+                
+            print("0x%s %s" % (byte.hex(), type))
+            
+        
 def main():
     args = sys.argv[1:]
-    
-    infile_name = ""
-    infile_specified = False
-    outfile_name = ""
-    
-    if len(args) == 0:
-        print("No arguments! Use -h for help.")
-    if "-h" in args or "--help" in args:
-        print("Usage:\n-h or --help to display help\n-if specify input file\n-of specify output file")
-    
-    if "-if" in args:
-        infile_index = args.index("-if")
-        if len(args) > infile_index+1:
-            infile_name = args[infile_index+1]
-            infile_specified = True
-        else:
-            print("No input file specified after '-if' flag!")
-            
-    if "-of" in args:
-            outfile_index = args.index("-of")
-            if len(args) > outfile_index+1:
-                outfile_name = args[outfile_index+1]
-            else:
-                print("No output file specified after '-of' flag!")
-                
-    if infile_specified:
-        convert(infile_name)
-        #bytedump(infile_name)
-        
+    convert(args[0])
     return 0
     
 if __name__ == "__main__":
